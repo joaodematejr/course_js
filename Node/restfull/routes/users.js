d49e46681f948f16ba628bcfd8358bd3a146714b
João Dematé <joao@MacBook-Pro-de-Joao.local>
2019-03-02 00:39:13 -0300

    Implementação body-parser, nedb

--- Node/restfull/routes/users.js
+++ Node/restfull/routes/users.js
@@ -1,3 +1,8 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
 module.exports = (app) => {
     app.get('/users', (req, res) => {
         res.statusCode = 200;
@@ -11,15 +16,16 @@ module.exports = (app) => {
         });
     });
 
-    app.get('/users/admin', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Context-Type', 'application/json');
-        res.json({
-            users: [{
-                name: 'João',
-                email: 'joao@email.com',
-                id: 1
-            }]
-        });
+    app.post('/users', (req, res) => {
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`Error:${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                res.status(200).json(user);
+            }
+        })
     });
 };
