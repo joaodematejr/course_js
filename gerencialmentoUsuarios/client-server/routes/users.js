1049592648542c2d23c9d47e533032e567b58e55
joaodematejr <joaodematejr@gmail.com>
2020-07-12 21:56:15 -0300

    Implementação PUT/DELETE/GET ID

--- gerencialmentoUsuarios/client-server/routes/users.js
+++ gerencialmentoUsuarios/client-server/routes/users.js
@@ -11,7 +11,39 @@ var client = restify.createJsonClient({
 router.get('/', function (req, res, next) {
   client.get('/users', function (err, request, response, obj) {
     assert.ifError(err);
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
+  });
+});
+
+/* GET ID. */
+router.get('/:id', function (req, res, next) {
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err);
+    res.json(obj);
+  });
+});
+
+/* PUT ID. */
+router.put('/:id', function (req, res, next) {
+  client.put(`/users/${req.params.id}`, req.body, function (err, request, response, obj) {
+    assert.ifError(err);
+    res.json(obj);
+  });
+});
+
+/* DELETE ID. */
+router.delete('/:id', function (req, res, next) {
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err);
+    res.json(obj);
+  });
+});
+
+/* POST ID. */
+router.post('/', function (req, res, next) {
+  client.post(`/users/`, req.body, function (err, request, response, obj) {
+    assert.ifError(err);
+    res.json(obj);
   });
 });
 
