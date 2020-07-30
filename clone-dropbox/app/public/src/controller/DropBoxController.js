425e4919cb4f9553cd13018912cf2c5faccb73ba
joaodematejr <joaodematejr@gmail.com>
2020-07-29 22:08:20 -0300

    Abrindo o arquivo

--- clone-dropbox/app/public/src/controller/DropBoxController.js
+++ clone-dropbox/app/public/src/controller/DropBoxController.js
@@ -514,7 +514,7 @@ class DropBoxController {
                this.openFolder();
                break;
             default:
-               window.open('/file?path=' + file.path);
+               window.open(file.path);
          }
       });
 
