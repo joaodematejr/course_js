1ff908b8f0d2e60bd4613d7908bdb8f254ea1fce
João Demate <joao.demate@alquimidiacorp.com.br>
2021-08-03 20:56:25 -0300

    Ações do microphone

--- ProjetoWhatsAppClone/src/controller/WhatsAppController.js
+++ ProjetoWhatsAppClone/src/controller/WhatsAppController.js
@@ -191,6 +191,33 @@ class WhatsAppController {
     this.el.btnCloseModalContacts.on("click", (e) => {
       this.el.modalContacts.hide();
     });
+
+    this.el.btnSendMicrophone.on('click', e => {
+      this.el.recordMicrophone.show();
+      this.el.btnSendMicrophone.hide();
+      this.startRecordMicrophoneTime();
+    });
+
+    this.el.btnCancelMicrophone.on('click', e => {
+      this.closeRecordMicrophone();
+    });
+
+    this.el.btnFinishMicrophone.on('click', e => {
+      this.closeRecordMicrophone();
+    });
+  }
+
+  startRecordMicrophoneTime() {
+    let start = Date.now();
+    this._recordMicrophoneInterval = setInterval(() => {
+      this.el.recordMicrophoneTimer.innerHTML = (Date.now() - start)
+    }, 100)
+  }
+
+  closeRecordMicrophone() {
+    clearInterval(this._recordMicrophoneInterval);
+    this.el.recordMicrophone.hide();
+    this.el.btnSendMicrophone.show();
   }
 
   closeAllMainPainel() {
